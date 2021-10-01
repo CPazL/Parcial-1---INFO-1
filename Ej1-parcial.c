fb3e58232584e5a18af2744668e1ba6e337bf9b0
CPazL <51858168+CPazL@users.noreply.github.com>
2021-10-01 20:32:32 -0300

    Add files via upload

---
+++ Ej1-parcial.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int ObtenerMayor(int *datos, int largo);
+void Estadistica(int *datos, int largo, float *promedio, float *desvio);
+/** Falto poner los prototipos de algunas funciones**/
+void vector_redimensionar(int *vec, int  nuevotam, int datonuevo);
+float promedio(int *vec, int tam );
+
+int main(){
+	int dato, i=0, mayor=0, n=0;
+
+	int *numeros=(*int)malloc(sizeof(int));
+	float promedio=0, desvio=0;
+	numeros=realloc(numeros,sizeof(int*) );
+
+	printf("------------Ingreso de datos---------------");
+	printf("Ingrese un numero, para finalizar ingrese el 0");
+	scanf("%d", &n);
+
+	while(n!=0){
+		i++;
+		scanf("%d",&dato);
+		vector_redimensionar(numeros, i+1, dato);
+		mayor=ObtenerMayor( numeros, i+1);
+		printf("EL mayor valor es : %d ", mayor);
+
+		/**Es un bucle que no termina ya que no pasa nada dentro**/
+		printf("Ingrese un numero, para finalizar ingrese el 0");
+		scanf("%d", &n);
+
+}
+free(numeros);
+return 0;
+}
+void vector_redimensionar(int *vec, int  nuevotam, int datonuevo){
+	void ** datos_nuevos = realloc(vec,nuevotam*sizeof(void*) );
+	free(vec);
+	vec=*datos_nuevos;
+	*(vec+(nuevotam-1))=datonuevo;
+
+}
+float promedio(int *vec, int tam ){
+	float prom=0;
+	int i;
+	for(i=0;i<tam;i++){
+		prom=+(*(vec+i));
+	}
+	prom=prom/tam;
+
+return prom;
+}
+float desvio(int *vec, int tam, float *prom){
+	float desvio=0;
+	int i=0;
+	for(i=0; i<tam; i++){
+		/***Error de codigo ,2 no va**/
+		desvio+=(*(vec+i)-*prom);
+	}
+	desvio=desvio/tam;
+	return desvio;
+}
+int ObtenerMayor(int *datos, int largo){
+	/**Error en el nombre de la variable datos , no se llama vec**/
+	int i, mayor=0;
+	for(i=0;i<largo;i++){
+		if((*(datos+i))>mayor){
+			mayor=(*(datos+i));
+		}
+	}
+return mayor;
+}
+void Estadistica(int *datos, int largo, float *prom, float *desv){
+	/**Se repiten los nombres y hay un error de compatiblidad con las variables y las funciones*/
+	*prom=promedio(datos, largo);
+	printf("\n El promedio es : %f ",*prom);
+	*desv=desvio(datos, largo, prom);
+	printf("\n El desvio  es : %f ",*desvio);
+
+}
+
---
+++ Ej3-parcial.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+/**Realizar un programa que recibe una frase por linea de comandos:
+ordernar_frase EstoEsUnaFraseLargaSinEspacios
+
+Y ordene las letras de la frase de menor a mayor obteniendo un resultado como el siguiente:
+
+EEEFLSUaaaaacegiinnooprrssssst
+
+Nótese que las mayúsculas aparecen antes que las minúsculas ya que en la tabla ASCII
+tienen un valor menor.
+*/
+int main(int argc, char *argv[], char *envp[]){
+int i=0;
+
+char*cadena;
+
+cadena=(char*)malloc(sizeof(envp[1]));
+strcpy(cadena, envp[1]);
+char temp;
+	while(*(cadena+i)!= '\0'){
+
+		if(*(cadena+i)>*(cadena+i+1)){
+			temp=*(cadena+i);
+			*(cadena+i)=*(cadena+i+1);
+			*(cadena+i+1)=*(cadena+i);
+
+		}
+	i++;
+	}
+printf("Cadena ordenada: %s ",cadena);
+
+return 0;
+
+
+}
